6a49dd12cd68816170c18af6ee34f0b47acd7a88
Jules Talloen <jules.talloen@ml6.eu>
2022-01-11 04:48:59 +0100

    Update README and default pipeline

--- deepstream/run.py
+++ deepstream/run.py
@@ -1,10 +1,10 @@
 from argparse import ArgumentParser
 
-from app.core import run_anonymization_pipeline
+from app.core import run_pipeline
 
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument("source_uri", type=str, help="The source URI")
     args = parser.parse_args()
-    run_anonymization_pipeline(args.source_uri)
+    run_pipeline(args.source_uri)
